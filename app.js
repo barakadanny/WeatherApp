939bfa4b04c3b1a4dd6f664641ffad309a2c7e2c
barakadanny <53088629+barakadanny@users.noreply.github.com>
2021-08-17 21:51:20 +0300

    auto complete feature

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ const TownDate = document.querySelector(".town-date");
 const TownHumidity = document.querySelector(".town-humidity");
 const Cards = document.querySelector(".cards");
 
+// auto complete selections
+const autoCompleteGround = document.querySelector(".auto-complete-ground");
+const autoCompleteItem = document.querySelector(".auto-complete-item");
+const searchSection = document.querySelector(".search-section");
+
 // variables
 let latitude, longitude, query;
 
@@ -239,3 +244,75 @@ navigation.addEventListener("click", changeTab, false)
 // "recent":[{location1Name:string, favorite:boolean:false}, {location2:string, favorite:boolean:false},...]
 // "favorites":[{location1Name:string, favorite:boolean:true}, {location1Name:string, favorite:boolean:true},...]
 // "suggestion":[{location1Name:string, favorite:boolean}, {location1Name:string, favorite:boolean},...]
+// auto complete feature function
+
+inputData.onkeyup =(e)=>{
+    let inData=e.target.value;
+    let emptyArray = [];
+    if(inData){
+        emptyArray = countries.filter((data)=>{
+            return data.toLocaleLowerCase().startsWith(inData.toLocaleLowerCase());
+        })
+        // console.log(emptyArray)
+        emptyArray = emptyArray.map((data)=>{
+            return data='<li>'+ data +'</li>'
+        })
+        // console.log(emptyArray)
+        autoCompleteGround.classList.add('auto-active')
+        showCountries(emptyArray)
+
+        let allList = autoCompleteGround.querySelectorAll('li')
+        for (let i = 0; i < allList.length; i++) {
+            // const element  = array[i];
+            allList[i].setAttribute("onclick", "select(this)")
+        }
+
+    }else{
+        autoCompleteGround.classList.remove('auto-active')
+    }
+}
+function select(element){
+    let selectedData = element.textContent
+    // console.log(selectedData)
+    inputData.value = selectedData
+    autoCompleteGround.classList.remove('auto-active')
+}
+
+function showCountries(list){
+    let listData;
+    if(!list.length){
+       let inData = inputData.value
+        listData = '<li>'+ inData + '</li>'
+    }else{
+        listData=list.join('')
+    }
+    autoCompleteGround.innerHTML = listData
+}
+
+// contries object
+var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla",
+"Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan",
+"Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda",
+"Bhutan","Bolivia","Bosnia & Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei",
+"Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands",
+"Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica",
+"Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica",
+"Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia",
+"Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies",
+"Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala",
+"Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India",
+"Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan",
+"Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho",
+"Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi",
+"Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia",
+"Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro",
+"Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria",
+"North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru",
+"Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda",
+"Saint Pierre & Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal",
+"Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia",
+"South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts & Nevis","St Lucia","St Vincent",
+"Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand",
+"Timor L'Este","Togo","Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan","Turks & Caicos","Tuvalu",
+"Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan",
+"Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
